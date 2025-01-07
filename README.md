2562cc7f0033078b7285c197d82b0cbaa38c1c2c
Samarth Verma <verma.samarth05@gmail.com>
2025-01-06 20:22:25 -0500

    Added read_articles

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-def main():
+- pip install newsapi-python
+- pip install python-dotenv
+- pip install newspaper3k TO_DO
+- pip install nltk TODO
+- pip install lxml TODO
+- pip install lxml_html_clean
+
+
+Websites that don’t work: 
+- yahoo.com
+- forbes.com
+- wsj.com
+- reuters.com
+- marketwatch.com
+- financialpost.com
+- thestreet.com
+- morningstar.com
+- barrons.com
--- analysis.py
+++ analysis.py
@@ -1,38 +1,58 @@
 from newsapi import NewsApiClient
 from dotenv import load_dotenv
 import os
+import sys
+from newspaper import Article
 
 load_dotenv()
 
 newsapi = NewsApiClient(os.getenv("NEWS_API_KEY"))
-domains = ["cnn.com", "bbc.com", "nytime.com", "wsj.com", "forbes.com", "bloomberg.com", "reuters.com", "marketwatch.com", "cnbc.com", "financialpost.com", "investopedia.com", "ft.com", "seekingalpha.com", "businessinsider.com", "thestreet.com", "morningstar.com", "zacks.com", "yahoo.com", "benzinga.com", "investing.com", "economist.com", "nasdaq.com", "kiplinger.com", "fool.com", "barrons.com", "marketplace.org"]
+
 
 def main():
-    company = "Tesla"
-    fetch_articles(company)
+    company = "Tesla" # customizable
+    start = "2024-12-25" # customizable
+    end = "2025-01-05" # customizable
+    domains = ["cnn.com", "bbc.com", "nytimes.com", "bloomberg.com", "cnbc.com", "investopedia.com",
+    "ft.com", "seekingalpha.com", "businessinsider.com", "zacks.com", "benzinga.com", "investing.com",
+    "economist.com", "nasdaq.com", "kiplinger.com", "fool.com", "marketplace.org"] # not customizable
+    
+    headlinesToLinks = (fetch_articles(company, domains, start, end))
+    titlesToContent = read_articles(headlinesToLinks)
 
-def fetch_articles(company_name):
+def fetch_articles(company_name, domains, start, end):
     my_dict = {}
     
     articles = newsapi.get_everything(
         q=company_name,
-        from_param='2025-01-01',  # Start date
-        to='2025-01-05',          # End date
+        from_param=start,  # Start date
+        to=end,          # End date
         language='en',            # Language of the articles
-        sort_by='publishedAt'     # Sort by most recent
+        sort_by='relevancy'     # Sort by relevancy
     )
     
     count = 0
     for article in articles['articles']:
         if (article['title'] != "[Removed]") and any(domain in article['url'] for domain in domains):
-            print(f"Title: {article['title']}")
-            print(f"URL: {article['url']}\n")
+            #print(f"Title: {article['title']}")
+            #print(f"URL: {article['url']}\n")
             my_dict[article['title']] = article['url']
             count += 1
-            if count == 5:  # Stop after 5 iterations
+            if count == 5:
                 break
     
     return my_dict
+
+def read_articles(my_dict):
+    articleToContent = {}
+    
+    for key, url in my_dict.items():  # Loop through key-value pairs
+        article = Article(url)
+        article.download()
+        article.parse()
+        articleToContent[key] = article.text
+
+    return articleToContent
 
 if __name__ == "__main__":
     main()
